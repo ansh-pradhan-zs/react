835986a35072dbda16b20ba402c08a6a06e22e77
Ansh <raramuri@Raramuris-MacBook-Pro.local>
2025-01-22 18:20:47 +0530

    commited accordion test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Accordion from "./components/Accordion";
 import Day9 from "./components/Day9";
 
 function App() {
   return (
     <main className="m-4">
-      <Day9 />
+      <Accordion />
     </main>
   );
 }
