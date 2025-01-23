0da6124c2872ce91addca618d890ad4cc45f0fe0
Ansh <raramuri@Raramuris-MacBook-Pro.local>
2025-01-23 11:45:25 +0530

    commiting day13 react test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import "./App.css";
-import Day9 from "./components/Day9";
+import Modal from "./components/Modal";
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
   return (
-    <main className="m-4">
-      <Day9 />
+    <main className="container">
+      <button onClick={() => setOpenModal(true)} className="open-modal-btn">
+        Open Modal
+      </button>
+      {openModal && <Modal setOpenModal={setOpenModal} />}
     </main>
   );
 }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,28 @@
+import { IoIosCloseCircle } from "react-icons/io";
+
+const Modal = ({ setOpenModal }) => {
+  function closeModal(e) {
+    if (e.target.id === "modal-container") {
+      setOpenModal(false);
+    }
+  }
+  return (
+    <div className="modal" id="modal-container" onClick={closeModal}>
+      <div className="modal-div" id="modal-div">
+        <div className="modal-title">
+          <span>Hello Its Me</span>
+          <IoIosCloseCircle size={30} onClick={() => setOpenModal(false)} />
+        </div>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure commodi
+          aspernatur quasi nihil laudantium non quis, nemo obcaecati tenetur
+          quos minus ratione inventore ex. Adipisci assumenda, quo excepturi
+          neque voluptatem consequuntur. Autem nam officia ea beatae culpa
+          necessitatibus recusandae molestiae consequuntur et? Quos, vero hic.
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
