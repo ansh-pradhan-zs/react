48a2f63da525b26c087ea8d84a66a41a43813022
Ansh <raramuri@Raramuris-MacBook-Pro.local>
2025-01-23 23:24:43 +0530

    commiting debounce assignment given by Bipin

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 import Modal from "./components/Modal";
+import SearchInput from "./components/SearchInput";
 
 function App() {
   const [openModal, setOpenModal] = useState(false);
   return (
     <main className="container">
-      <button onClick={() => setOpenModal(true)} className="open-modal-btn">
+      {/* <button onClick={() => setOpenModal(true)} className="open-modal-btn">
         Open Modal
       </button>
-      {openModal && <Modal setOpenModal={setOpenModal} />}
+      {openModal && <Modal setOpenModal={setOpenModal} />} */}
+      <SearchInput />
     </main>
   );
 }
