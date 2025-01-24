5f8c67cecbe834c4105e9145f41dc01bddadf3dd
Ansh <raramuri@Raramuris-MacBook-Pro.local>
2025-01-25 02:32:19 +0530

    commiting throttling assignment given by Bipin

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import "./App.css";
-import Day9 from "./components/Day9";
 
 function App() {
+  // function throttle(func, delay) { // ? using timeoutId
+  //   let timeoutId;
+  //   return () => {
+  //     if (!timeoutId) {
+  //       func(); // ? api call
+  //       timeoutId = setTimeout(() => {
+  //         timeoutId = null;
+  //       }, delay);
+  //     }
+  //   };
+  // }
+
+  function throttle(func, delay) {
+    // ? using locked variable
+    let locked = false;
+    return () => {
+      if (!locked) {
+        func(); // ? api call
+        locked = true;
+        setTimeout(() => {
+          locked = false;
+        }, delay);
+      }
+    };
+  }
+
   return (
-    <main className="m-4">
-      <Day9 />
+    <main className="m-4 flex flex-row items-center gap-5">
+      <button
+        className="bg-blue-500 px-5 py-1 rounded-md"
+        onClick={throttle(() => {
+          let timeClicked = new Date();
+          console.log(timeClicked);
+        }, 1000)}
+      >
+        Search
+      </button>
     </main>
   );
 }
