5ac3c7b3443aa63e0d725c80aff59c78cf0e4e1a
Ansh <raramuri@Raramuris-MacBook-Pro.local>
2025-01-31 04:06:34 +0530

    made some small change in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <main>
-        <Day9 />
+        {/* <Day9 />
         <DiceRoll />
         <ParentDetailCard
           title={"SDE-1"}
@@ -19,7 +19,7 @@ function App() {
             "https://plus.unsplash.com/premium_photo-1689539137236-b68e436248de?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           }
         />
-        <Accordion />
+        <Accordion /> */}
         <Day13 />
       </main>
     </>
